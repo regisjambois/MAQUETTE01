6653e03c24522a8f0f14f79b01cdc410764eb89f
regisjambois <33594622+regisjambois@users.noreply.github.com>
2018-09-22 22:17:29 +0200

    20180922

---
+++ src/app/cardtest/cardtest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardtest',
+  templateUrl: './cardtest.component.html',
+  styleUrls: ['./cardtest.component.scss']
+})
+export class CardtestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
